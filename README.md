531f8eac97c8640b5efad49316ba935298a93e4f
Noor un nisa Memon <163582696+Whoiznoor1@users.noreply.github.com>
2024-11-11 23:22:17 +0500

    my portfolio

--- README.md
+++ README.md
@@ -40,3 +40,4 @@ Check out our [Next.js deployment documentation](https://nextjs.org/docs/app/bui
 "# Portfolio-Milestone-2-2024" 
 "# Milestone-Portfolio" 
 "# Milestone-2-portfolio" 
+"# Milestone-Portfolio" 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,7 @@ import type { Config } from "tailwindcss";
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/Components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
@@ -17,3 +17,5 @@ const config: Config = {
   plugins: [],
 };
 export default config;
+
+
